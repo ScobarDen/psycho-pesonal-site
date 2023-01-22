95f573395f698ec1f9a8df7ab9a7e7b55b4b4e46
ScobarDen <gdv-ps@yandex.ru>
2023-01-22 20:06:39 +0300

    пока что закончил навбар fix

--- src/components/ui/Header/index.tsx
+++ src/components/ui/Header/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { AppBar, IconButton, Tab, Tabs, Toolbar, Typography } from '@mui/material';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
+import PsychologyIcon from '@mui/icons-material/Psychology';
 import { useSelector } from 'react-redux';
 import { selectTheme, Theme, toggleThemeMode } from '../../../redux/theme';
 import { useAppDispatch } from '../../../redux/hooks';
@@ -19,30 +20,29 @@ const Header: React.FC = () => {
   };
 
   return (
-    <div className={styles.header}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" className={styles.title}>
-            DAVAR
-          </Typography>
-          <Tabs value={value} onChange={handleChangeTab}>
-            <Tab label="Онлайн запись" />
-            <Tab label="Личный прем" />
-            <Tab label="Обо мне" />
-            <Tab label="Статьи" />
-            <Tab label="Войти/Регистрация" />
-          </Tabs>
-          <IconButton
-            onClick={handleToggleTheme}
-            edge="start"
-            className={styles.themeButton}
-            color="inherit"
-            aria-label="menu">
-            {mode === Theme.LIGHT ? <DarkModeIcon /> : <LightModeIcon />}
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <AppBar position="fixed">
+      <Toolbar>
+        <PsychologyIcon />
+        <Typography variant="h6" className={styles.title}>
+          DAVAR
+        </Typography>
+        <Tabs value={value} onChange={handleChangeTab}>
+          <Tab label="Онлайн запись" />
+          <Tab label="Личный прем" />
+          <Tab label="Обо мне" />
+          <Tab label="Статьи" />
+          <Tab label="Войти/Регистрация" />
+        </Tabs>
+        <IconButton
+          onClick={handleToggleTheme}
+          edge="start"
+          className={styles.themeButton}
+          color="inherit"
+          aria-label="menu">
+          {mode === Theme.LIGHT ? <DarkModeIcon /> : <LightModeIcon />}
+        </IconButton>
+      </Toolbar>
+    </AppBar>
   );
 };
 
