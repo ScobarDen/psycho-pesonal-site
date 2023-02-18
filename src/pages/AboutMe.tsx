d23dcee4041cab0f97a8270316f5347ed4178eae
ScobarDen <gdv-ps@yandex.ru>
2023-02-18 13:49:16 +0300

    add scroll

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { workWithArray } from '../info';
 import { AboutCard } from '../components/common/AboutCard';
-import { Box, Typography } from '@mui/material';
+import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material';
+import { Link as ScrollLink } from 'react-scroll';
 
 export const AboutMe: React.FC = () => {
   return (
     <>
       <Box sx={{ p: 3 }}>
+        <ScrollLink to="section1" smooth={true} duration={500}>
+          <Button color="inherit">C чем я работаю</Button>
+        </ScrollLink>
+      </Box>
+
+      <Box id="section1" sx={{ p: 3 }}>
         <Typography variant="h3" sx={{ marginBottom: 3 }}>
           С чем я работаю
         </Typography>
