118e0dfd8b472ffaf66a18da0dab78efd012c042
ScobarDen <gdv-ps@yandex.ru>
2023-03-25 15:13:00 +0300

    add navigate

--- src/MyRouter.tsx
+++ src/MyRouter.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Route, Routes } from "react-router-dom";
-import { AdminLayout, AuthenticationLayout, UsersLayout } from "./layouts";
-import { NotFoundPage } from "./pages/NotFoundPage";
-import { LoginPage } from "./pages/LoginPage";
-import { RegisterPage } from "./pages/RegisterPage";
-import { AboutMe } from "./pages/AboutMe";
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { AdminLayout, AuthenticationLayout, UsersLayout } from './layouts';
+import { NotFoundPage } from './pages/NotFoundPage';
+import { LoginPage } from './pages/LoginPage';
+import { RegisterPage } from './pages/RegisterPage';
+import { AboutMe } from './pages/AboutMe';
 
 export const MyRouter: React.FC = () => {
   const isAdmin = false; // todo: сделать получение этого значения с бэкенда
@@ -14,17 +14,15 @@ export const MyRouter: React.FC = () => {
         <Route path="/admin" element={<AdminLayout />}></Route>
       ) : (
         <Route path="/" element={<UsersLayout />}>
-          <Route path="" element={<AboutMe/>}/>
+          <Route path="" element={<AboutMe />} />
         </Route>
       )}
-      <Route path="/auth" element={<AuthenticationLayout/>}>
-        <Route path="login" element={<LoginPage/>}/>
-        <Route path="register" element={<RegisterPage/>}/>
+      <Route path="/auth" element={<AuthenticationLayout />}>
+        <Route path="login" element={<LoginPage />} />
+        <Route path="register" element={<RegisterPage />} />
       </Route>
-      <Route
-        path="*"
-        element={<NotFoundPage/>}
-      />
+      <Route path="/notfound" element={<NotFoundPage />} />
+      <Route path="*" element={<Navigate to={'/notfound'} />} />
     </Routes>
   );
 };
